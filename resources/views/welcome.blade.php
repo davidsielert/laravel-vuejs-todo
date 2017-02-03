2b66548f47d60b8f3e869f8b6b97edbd49d2a3e4
David Sielert <davidmirv@gmail.com>
2017-02-03 12:31:04 -0800

    Updated welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
+
+    <div class="container">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                Welcome to David Sielert's Laravel and Vue.JS example site, Register and login to get started!
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
